e6bcb80ec386b2db08381f9362a16eb6af157a94
Yessica54 <y3ssic454@gmail.com>
2025-05-23 10:50:59 -0500

    Update Home.jsx
    Update IP

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -54,7 +54,7 @@ const Home = () => {
         }
         const params = new URLSearchParams(paramQuery).toString();
 
-        fetch(`http://ec2-3-135-221-63.us-east-2.compute.amazonaws.com:8080/ms-books-catalogue/books?${params}`)
+        fetch(`http://10.0.2.102:8080/ms-books-catalogue/books?${params}`)
             .then((res) => {
                 if (!res.ok) {
                     throw new Error("Failed to fetch books");
